d150141ef565a52df3df0135488c314b041ac306
spencer-gray <34588305+spencer-gray@users.noreply.github.com>
2018-06-08 17:25:17 -0400

    MLB exception implemented

--- espn_scrape.py
+++ espn_scrape.py
@@ -16,8 +16,7 @@ def to_abbr(item):
                   'oklahoma_city': 'okc', 'orlando': 'orl', 'ottawa': 'ott','philadelphia': 'phi', 'phoenix': 'phx', 'pittsburgh': 'pit', 'portland': 'por', 
                   'sacramento': 'sac', 'san_antonio': 'sa', 'san_francisco': 'sf', 'san_jose': 'sj', 'seattle': 'sea', 'st_louis': 'stl', 'tampa_bay': 'tb', 
                   'tennessee': 'ten', 'toronto': 'tor', 'utah': 'utah', 'vancouver': 'van', 'vegas': 'vgs', 'washington': 'wsh', 'winnipeg': 'wpg'}
-
-    #if city name has a team in the big 4 NA leauges
+    
     if item in nameToAbbr:   
         return nameToAbbr[item]
     else:
@@ -25,9 +24,9 @@ def to_abbr(item):
 
 
 # writes the specified data to an appropriately named csv file #
-def write_to_csv(file, data, url):
+def write_to_csv(file, data, writeType):
         
-    with open(file+'.csv', 'w') as csvfile:
+    with open(file+'.csv', writeType) as csvfile:
         writeCSV = csv.writer(csvfile, delimiter=',', lineterminator = '\n')
         for table in data:
             table_rows = table.find_all('tr')#[1:] #[1:] will remove the title of the current table (espn stores it as a tr)
@@ -35,10 +34,9 @@ def write_to_csv(file, data, url):
                 td = tr.find_all('td')
                 row = [i.text for i in td]
                 
-                writeCSV.writerow(row)
+                writeCSV.writerow(row)    
 
-
-parser = argparse.ArgumentParser(description='scrape specific team stats from ESPN (4 Major NA Sports)')
+parser = argparse.ArgumentParser(description='scrape specific team stats from espn (works for nhl/nba/nfl/mlb)')
 
 parser.add_argument("city_input", help="the city name to be searched")
 parser.add_argument("yr_input", help="the year to be searched")
@@ -53,6 +51,26 @@ league = args.league_input.lower()
 
 city_abr = to_abbr(city)
 
+### if mlb open the four separate pages and extract the data###
+mlb_pages = ['batting', 'pitching', 'fielding', 'splits']
+
+if league == 'mlb':
+    count = 0
+    for page in mlb_pages:
+        url = 'http://www.espn.com/'+league+'/team/stats/'+mlb_pages[count]+'/_/name/'+city_abr+'/year/'+year+'/seasontype/2'
+        sauce = urllib.request.urlopen(url).read()
+        soup = bs.BeautifulSoup(sauce, 'lxml')
+        tables = soup.find_all('table', {'class': "tablehead"})
+        if bool(tables) == False:
+            sys.exit('No table objects found on the requested page, verify info provided is accurate')
+        file_name = city_abr+'_'+league+'_team_stats_'+previous_year+'-'+year
+        if count == 0:
+            write_to_csv(file_name, tables, 'w')
+        else:
+            write_to_csv(file_name, tables, 'a')
+        count+=1
+    sys.exit()
+        
 url = 'http://www.espn.com/'+league+'/team/stats/_/name/'+city_abr+'/year/'+year+'/seasontype/2'
 file_name = city_abr+'_'+league+'_team_stats_'+previous_year+'-'+year
 
@@ -61,4 +79,4 @@ sauce = urllib.request.urlopen(url).read()
 soup = bs.BeautifulSoup(sauce, 'lxml')
 tables = soup.find_all('table', {'class': "tablehead"})   
 
-write_to_csv(file_name, tables, url)
+write_to_csv(file_name, tables, 'w')
